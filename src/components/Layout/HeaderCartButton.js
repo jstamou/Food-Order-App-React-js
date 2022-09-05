6117f2fc05f7fe7671163c03e328983f3c6f865e
Giannis Stamou <68112697+jstamou@users.noreply.github.com>
2022-09-05 13:41:18 +0300

    Add files via upload

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHiglighted] = useState(false);
+  const CartCtx = useContext(CartContext);
+  const { items } = CartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHiglighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHiglighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
